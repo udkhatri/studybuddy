9b2cfb33ab511cb14329370aa1e325525335b2e7
udkhatri <udayskhatri@gmail.com>
2021-02-16 11:37:43 +0530

    Navigationbar improved

--- scr/navigation/bottomTab.js
+++ scr/navigation/bottomTab.js
@@ -1,42 +1,46 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, TouchableOpacity} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/UI/HomeScreen';
-import TutorialScreen from '../screens/UI/tutorialScreen'
-import ProfileScreen from '../screens/UI/profileScreen'
-import UploadScreen from '../screens/UI/UploadScreen'
-import ChatScreen from '../screens/UI/chatScreen'
+import TutorialScreen from '../screens/UI/tutorialScreen';
+import ProfileScreen from '../screens/UI/profileScreen';
+import UploadScreen from '../screens/UI/UploadScreen';
+import ChatScreen from '../screens/UI/chatScreen';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import Feather from 'react-native-vector-icons/Feather';
 import TikTok from '../Images/dummy-logo.png';
+import {useTheme} from 'react-native-paper';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 const Tab = createBottomTabNavigator();
 
 const HomeBottomTabNavigator = () => {
+  const {colors} = useTheme();
   return (
     <Tab.Navigator
       tabBarOptions={{
         tabStyle: {
           backgroundColor: '#fff',
         },
-        activeTintColor: '#3498db',
+        activeTintColor: colors.primary,
         inactiveTintColor: '#999',
         //showLabel: false,
-        style:{
+        style: {
           borderTopWidth: 0,
           paddingTop: 3,
           paddingBottom: 4,
-          height: 60,          
-        }
+          height: 55,
+        },
       }}>
       <Tab.Screen
         name={'Home'}
         component={HomeScreen}
         options={{
           tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
+          tabBarIcon: ({color, size}) => (
             <AntDesign name="home" color={color} size={size} />
           ),
         }}
@@ -55,8 +59,14 @@ const HomeBottomTabNavigator = () => {
         component={UploadScreen}
         options={{
           tabBarIcon: ({color, size}) => (
-            <Ionicons name="add-circle-outline" color={color} size={size*2.5} style={{paddingBottom:20}}/>
-          ),
+            <TouchableWithoutFeedback>
+              <Image
+                style={{height:75, width:75, marginBottom:"50%"}}
+                source={require('../Images/Plus.png')}
+            />
+            </TouchableWithoutFeedback>
+             
+          )
           //tabBarLabel: () => null,
         }}
       />
